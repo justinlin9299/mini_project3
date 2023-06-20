2a77076f4586900c1502e7916a19858d72dd906d
justinlin9299 <136868011+justinlin9299@users.noreply.github.com>
2023-06-20 18:07:10 +0800

    evaluate updated

--- src/state/state.cpp
+++ src/state/state.cpp
@@ -17,43 +17,6 @@ int chess_score[7] = {
  * @return int 
  */
 
-// int State::evaluate(){
-//   // [TODO] design your own evaluation function
-//   auto white_board = this->board.board[0];
-//   auto black_board = this->board.board[1];
-//   int white_score = 0;
-//   int black_score = 0;
-
-//   for (int i = 0; i < BOARD_H; i++) {
-//     for (int j = 0; j < BOARD_W; j++) {
-//       white_score += chess_score[this->board.board[0][i][j]];
-//       black_score += chess_score[this->board.board[1][i][j]];
-//       if (i == 3  && j == 2) {
-//         if (white_board[i][j] == '1')
-//           white_score += 6;
-//         else if (white_board[i][j] == '3')
-//           white_score += 4;
-//       }
-//       if (i == 2 && j == 2) {
-//         if (black_board[i][j] == '1')
-//           black_score += 6;
-//         else if (black_board[i][j] == '3')
-//           black_score += 4;
-//       }
-//     }
-//   }
-//   if (white_board[5][0] != '2')
-//     white_score += 3;
-//   if (black_board[0][4] != '2')
-//     black_score += 3;
-
-//   if (white_board[5][4] == '6')
-//     white_score += 5;
-//   if (black_board[0][0] == '6')
-//     black_score += 5;
-
-//   return white_score - black_score;
-// }
 
 int State::evaluate(){
   // [TODO] design your own evaluation function
@@ -67,25 +30,25 @@ int State::evaluate(){
       white_score += chess_score[white_board[i][j]];
       black_score += chess_score[black_board[i][j]];
 
-      // if (i == 3 && j == 2) {
-      //   if (white_board[i][j] != '0')
-      //     white_score += 5;
-      //   if (black_board[i][j] != '0')
-      //     black_score += 5;
-      // }
-      // if (i == 2 && j == 2) {
-      //   if (white_board[i][j] != '0')
-      //     white_score += 5;
-      //   if (black_board[i][j] != '0')
-      //     black_score += 5;
-      // }
-      // if ((i == 2 && j == 1) || (i == 3 && j == 3) || (i == 2 && j == 3) || (i == 3 && j == 3) \
-      //  || (i == 1 && j == 1) || (i == 1 && i == 3) || (i == 4 && j == 1) || (i == 4 && j == 3)){
-      //   if (white_board[i][j] != '0')
-      //     white_score += 1;
-      //   if (black_board[i][j] != '0')
-      //     black_score += 1;
-      // }
+      if (i == 3 && j == 2) {
+        if (white_board[i][j] != 0)
+          white_score += 2;
+        if (black_board[i][j] != 0)
+          black_score += 2;
+      }
+      if (i == 2 && j == 2) {
+        if (white_board[i][j] != 0)
+          white_score += 2;
+        if (black_board[i][j] != 0)
+          black_score += 2;
+      }
+      if ((i == 2 && j == 1) || (i == 3 && j == 3) || (i == 2 && j == 3) || (i == 3 && j == 3) \
+       || (i == 1 && j == 1) || (i == 1 && i == 3) || (i == 4 && j == 1) || (i == 4 && j == 3)){
+        if (white_board[i][j] != 0)
+          white_score += 1;
+        if (black_board[i][j] != 0)
+          black_score += 1;
+      }
     }
   }
   
